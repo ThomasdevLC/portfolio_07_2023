7592ffdc4b28753ebd6b2f6ede4937a17da317bb
ThomasdevLC <thomas.lecam@gmail.com>
2023-06-26 21:55:55 +0200

    video container + play button

--- src/pages/works/Groupomania.js
+++ src/pages/works/Groupomania.js
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import WorksComponent from "../../components/WorksComponent";
-// import video from "../../../assets/videos/groupomania-video.mp4";
+import video from "../../assets/videos/groupomania-video.mp4";
 
 const Groupomania = () => {
+  const videoRef = useRef(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const toggleVideo = () => {
+    if (isPlaying) {
+      videoRef.current.pause();
+    } else {
+      videoRef.current.play();
+    }
+    setIsPlaying(!isPlaying);
+  };
+
   return (
     <section className="container">
       <WorksComponent />
+      <button className="container__video-btn" onClick={toggleVideo}>
+        {isPlaying ? "ll Pause" : "► Play video"}
+      </button>
+      <video
+        className="container__video"
+        width="100%"
+        src={video}
+        type="video/mp4"
+        loop
+        ref={videoRef}
+      ></video>
     </section>
   );
 };
