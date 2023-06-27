adf58879c1ee2cb607994edc9ae138f1f9ae19cb
ThomasdevLC <thomas.lecam@gmail.com>
2023-06-27 16:09:24 +0200

    intro  + pitch  rougeraie + groupomania

--- src/worklist/worklistData.js
+++ src/worklist/worklistData.js
@@ -8,9 +8,9 @@ const worklist = [
     tags: ["Vue", "Sass", "NodeJs", "Figma"],
     link: "ferme-rougeraie",
     intro:
-      "Dans le cadre d'un des projet Openclassrooms, il était demandé d'intégrer le design d'une maquette figma avec la technologie React.js. Ce projet fut ma première application React.js avec l'utilisation de react-router et typescript.",
+      " Site e-commerce pour une productrice de produits bio qui permet aux clients de réserver facilement des produits.",
     pitch:
-      "Le site de Kasa a été codé il y a maintenant plus de 10 ans en ASP.NET avec un code legacy important. Laura, la CTO, a donc lancé une refonte totale pour passer à une stack complète en JavaScript avec NodeJS côté Back-end, et React côté Front-end. Kasa en a également profité pour commander de nouvelles maquettes auprès de son designer habituel, qui est en freelance.",
+      "Le site a été développé à l'aide de Vue 3 pour la partie frontend, NodeJS pour la partie Backend et MongoDb pour la bade de données de produits. J'ai implémenté un panier pour que les utilisateurs puissent ajouter des produits et passer leur commande en toute simplicité. En tant qu'administrateur, Léa, la productrice, peut se connecter au site et accéder à un résumé complet de toutes les commandes. La page d'administration lui permet également de créer les cartes produits et de gérer leur affichage sur le site.",
     url: "http://test.com/",
   },
   {
@@ -38,7 +38,7 @@ const worklist = [
     intro:
       "Dans le cadre d'un des projet Openclassrooms, il était demandé d'intégrer la maquette figma d'une plateforme de location de logements entre particuliers avec la technologie React.js et l'utilisation de react-router.",
     pitch:
-      "Le site  a été codé il y a maintenant plus de 10 ans avec un code legacy important. L'entreprise a lancé une refonte totale pour passer à une stack complète en JavaScript avec NodeJS côté Back-end, et React côté Front-end. Le site est responsive et les composants sont modulaires et réutilisables. Données mockées en Json.",
+      "Le site originale a été codé il y a plus de 10 ans avec un code legacy important. L'entreprise a lancé une refonte totale pour passer à une stack complète en JavaScript avec NodeJS côté Back-end, et React côté Front-end. Le site est responsive et les composants sont modulaires et réutilisables.",
     url: "http://test.com/",
   },
   {
@@ -50,9 +50,9 @@ const worklist = [
     tags: ["React", "Sass", "NodeJs"],
     link: "groupomania",
     intro:
-      "Dans le cadre d'un des projet Openclassrooms, il était demandé d'intégrer le design d'une maquette figma avec la technologie React.js. Ce projet fut ma première application React.js avec l'utilisation de react-router et typescript.",
+      " Réseau social interne pour les employés d'une entreprise réalisé durant ma formation Openclassrooms.",
     pitch:
-      "Le site de Kasa a été codé il y a maintenant plus de 10 ans en ASP.NET avec un code legacy important. Laura, la CTO, a donc lancé une refonte totale pour passer à une stack complète en JavaScript avec NodeJS côté Back-end, et React côté Front-end. Kasa en a également profité pour commander de nouvelles maquettes auprès de son designer habituel, qui est en freelance.",
+      "Le projet fullstack  comprend une page de connexion permettant à l'utilisateur de se connecter, ou bien de créer un compte, une fonctionnalité de création de messages ainsi qu' une page listant les posts et commentaires.",
     url: "http://test.com/",
   },
 ];
