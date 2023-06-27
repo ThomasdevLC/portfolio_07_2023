e37bf27b455d3b738e36a43ef740ecd527081fa6
ThomasdevLC <thomas.lecam@gmail.com>
2023-06-27 14:59:20 +0200

    intro + pitch kasa +pwa

--- src/worklist/worklistData.js
+++ src/worklist/worklistData.js
@@ -22,9 +22,9 @@ const worklist = [
     tags: ["Vue", "Sass", "Figma"],
     link: "pwa",
     intro:
-      "Dans le cadre d'un des projet Openclassrooms, il était demandé d'intégrer le design d'une maquette figma avec la technologie React.js. Ce projet fut ma première application React.js avec l'utilisation de react-router et typescript.",
+      "Lors de mon stage dans l'entreprise GCA, j'ai développé une application mobile first conçue spécifiquement pour répondre aux besoins des équipes commerciales.",
     pitch:
-      "Le site de Kasa a été codé il y a maintenant plus de 10 ans en ASP.NET avec un code legacy important. Laura, la CTO, a donc lancé une refonte totale pour passer à une stack complète en JavaScript avec NodeJS côté Back-end, et React côté Front-end. Kasa en a également profité pour commander de nouvelles maquettes auprès de son designer habituel, qui est en freelance.",
+      "Ce projet, développé en utilisant Vue 3 et configurée avec Vite, a été créé dans le but de tenir les commerciaux informés de leurs chiffres de vente et de leurs objectifs en temps réel. En adoptant une approche PWA (Progressive Web App), l'application peut  être installée directement sur l'écran d'accueil des utilisateurs, offrant ainsi une expérience similaire à celle d'une application native.",
     url: "http://test.com/",
   },
   {
@@ -36,9 +36,9 @@ const worklist = [
     tags: ["React", "Sass"],
     link: "kasa",
     intro:
-      "Dans le cadre d'un des projet Openclassrooms, il était demandé d'intégrer le design d'une maquette figma avec la technologie React.js. Ce projet fut ma première application React.js avec l'utilisation de react-router et typescript.",
+      "Dans le cadre d'un des projet Openclassrooms, il était demandé d'intégrer la maquette figma d'une plateforme de location de logements entre particuliers avec la technologie React.js et l'utilisation de react-router.",
     pitch:
-      "Le site de Kasa a été codé il y a maintenant plus de 10 ans en ASP.NET avec un code legacy important. Laura, la CTO, a donc lancé une refonte totale pour passer à une stack complète en JavaScript avec NodeJS côté Back-end, et React côté Front-end. Kasa en a également profité pour commander de nouvelles maquettes auprès de son designer habituel, qui est en freelance.",
+      "Le site  a été codé il y a maintenant plus de 10 ans avec un code legacy important. L'entreprise a lancé une refonte totale pour passer à une stack complète en JavaScript avec NodeJS côté Back-end, et React côté Front-end. Le site est responsive et les composants sont modulaires et réutilisables. Données mockées en Json.",
     url: "http://test.com/",
   },
   {
