dc707dff43992514feccceab4d1e6ab44be984eb
ThomasdevLC <thomas.lecam@gmail.com>
2023-06-30 13:59:39 +0200

    tags section responsive

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -21,7 +21,7 @@ const Home = () => {
   };
 
   return (
-    <div className="home br">
+    <div className="home">
       <div className="home__content">
         <div className="home__intro">
           <div className="home__intro__pic">
