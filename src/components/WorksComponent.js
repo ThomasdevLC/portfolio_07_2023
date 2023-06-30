d8324180b8e00a0502789008fcf6a3d3b832b8fe
ThomasdevLC <thomas.lecam@gmail.com>
2023-06-30 15:09:15 +0200

    page farm + kasa images

--- src/components/WorksComponent.js
+++ src/components/WorksComponent.js
@@ -18,7 +18,7 @@ const WorksComponent = () => {
   }
 
   return (
-    <section className="container">
+    <section className="container br">
       <NavLink to="/">
         <div className="container__backpage"> &#x2191; PORTFOLIO</div>
       </NavLink>
--- src/pages/works/FermeRougeraie.js
+++ src/pages/works/FermeRougeraie.js
@@ -1,10 +1,24 @@
 import React from "react";
 import WorksComponent from "../../components/WorksComponent";
+import farmProducts from "../../assets/images/ferme_rougeraie_about.png";
+import farmAbout from "../../assets/images/ferme_rougeraie_products.png";
 
 const FermeRougeraie = () => {
   return (
-    <div>
+    <div className="farm__container">
       <WorksComponent />
+      <div className="farm__description">
+        <img
+          className="image-one"
+          src={farmProducts}
+          alt="image projet ferme de la rougeraie page a propos"
+        />
+        <img
+          className="image-two"
+          src={farmAbout}
+          alt="image projet ferme de la rougeraie page produits"
+        />
+      </div>
     </div>
   );
 };
