f8726627d7a582e8fd3782d7932ec305651e9749
ThomasdevLC <thomas.lecam@gmail.com>
2023-07-06 17:10:49 +0200

    header bacground

--- src/components/HeaderNav.js
+++ src/components/HeaderNav.js
@@ -1,11 +1,16 @@
-import React, { useEffect } from "react";
-
+import React from "react";
 import cv from "../assets/pdf/cv.pdf";
+import background from "../assets/images/header_background.jpg";
 
 const HeaderNav = () => {
   return (
-    <div className="header">
-      <p className="header__title">Portfolio 2023</p>
+    <div
+      className="header"
+      style={{ background: `url(${background}) rgba(255, 255, 255, 0.1)` }}
+    >
+      <p className="header__title">
+        Portfolio <span>2023</span>
+      </p>
       <a className="header__cv" href={cv} target="_blank" rel="noreferrer">
         MON CV
       </a>
