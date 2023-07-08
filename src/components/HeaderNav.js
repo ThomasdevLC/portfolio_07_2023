3f4b2b9771aee71a253e400d720041890043d746
ThomasdevLC <thomas.lecam@gmail.com>
2023-07-08 17:28:11 +0200

    header boder gsap animation

--- src/components/HeaderNav.js
+++ src/components/HeaderNav.js
@@ -1,9 +1,10 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import cv from "../assets/pdf/cv.pdf";
 import background from "../assets/images/header_background.jpg";
 import SplitType from "split-type";
 
 const HeaderNav = () => {
+  const borderRef = useRef(null);
   useEffect(() => {
     const title = SplitType.create(".header__box__title");
     const splitTitle = title.chars;
@@ -17,23 +18,33 @@ const HeaderNav = () => {
       duration: 1,
       delay: 0.5,
     });
+
+    const borderElement = borderRef.current;
+    gsap.fromTo(
+      borderElement,
+      { width: "0%" }, // Définir la largeur initiale à 0%
+      { width: "100%", duration: 1, ease: "power2.out", delay: 0.3 } // Définir la largeur finale à 100%
+    );
   }, []);
 
   return (
-    <div
-      className="header"
-      style={{
-        background: `linear-gradient(rgba(255, 255, 255, 0.1), white), url(${background})`,
-      }}
-    >
-      <div className="header__box">
-        <p className="header__box__title">Portfolio 2023</p>
+    <>
+      <div
+        className="header"
+        style={{
+          background: `linear-gradient(rgba(255, 255, 255, 0.1), white), url(${background})`,
+        }}
+      >
+        <div className="header__box">
+          <p className="header__box__title">Portfolio 2023</p>
+        </div>
+        {/* <span>2023</span> */}
+        <a className="header__cv" href={cv} target="_blank" rel="noreferrer">
+          MON CV
+        </a>
       </div>
-      {/* <span>2023</span> */}
-      <a className="header__cv" href={cv} target="_blank" rel="noreferrer">
-        MON CV
-      </a>
-    </div>
+      <div ref={borderRef} className="border"></div>
+    </>
   );
 };
 
