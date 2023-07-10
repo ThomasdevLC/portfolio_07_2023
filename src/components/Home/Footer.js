d0ed19769e5906ab818b955718b3c32a2815142e
ThomasdevLC <thomas.lecam@gmail.com>
2023-07-10 21:10:03 +0200

    styles fixed

--- src/components/Home/Footer.js
+++ src/components/Home/Footer.js
@@ -1,8 +1,14 @@
 import React from "react";
+import background from "../../assets/images/header_background.jpg";
 
 const Footer = () => {
   return (
-    <div className="footer">
+    <div
+      className="footer"
+      style={{
+        background: `linear-gradient(rgba(255, 255, 255, 0.1), white), url(${background})`,
+      }}
+    >
       <div className="footer__border"></div>
     </div>
   );
