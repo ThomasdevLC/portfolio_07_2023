8673d3519a7990e5d7335c8422414490884aecda
ThomasdevLC <thomas.lecam@gmail.com>
2023-07-11 11:56:48 +0200

    back element render fixed

--- src/components/works/BackBtn.js
+++ src/components/works/BackBtn.js
@@ -2,22 +2,33 @@ import React, { useEffect, useRef } from "react";
 import backbtn from "../../assets/images/backbtn.png";
 import { NavLink } from "react-router-dom";
 
-// index.js
-window.onscroll = function () {
-  scrollRotate();
-};
-
-const scrollRotate = () => {
-  let image = document.getElementById("backhome");
-  image.style.transform = "rotate(" + window.scrollY / 2 + "deg)";
-};
-
 const BackBtn = () => {
+  const imageRef = useRef(null);
+
+  useEffect(() => {
+    const scrollRotate = () => {
+      if (imageRef.current) {
+        imageRef.current.style.transform = `rotate(${window.scrollY / 2}deg)`;
+      }
+    };
+
+    window.addEventListener("scroll", scrollRotate);
+
+    return () => {
+      window.removeEventListener("scroll", scrollRotate);
+    };
+  }, []);
+
   return (
     <div>
       <NavLink to="/">
         <div className="backpage">
-          <img src={backbtn} id="backhome" alt="bouton retour page d'accueil" />
+          <img
+            src={backbtn}
+            id="backhome"
+            ref={imageRef}
+            alt="bouton retour page d'accueil"
+          />
           <div className="backpage__content">
             <span className="backpage__content__icon">
               <i class="fa-solid fa-arrow-left"></i>
