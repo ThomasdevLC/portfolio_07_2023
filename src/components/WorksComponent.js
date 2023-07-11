c003f5d1e45810359ad28f93c42e9c25427ce6a1
ThomasdevLC <thomas.lecam@gmail.com>
2023-07-11 12:57:59 +0200

    anim height  works

--- src/components/WorksComponent.js
+++ src/components/WorksComponent.js
@@ -25,7 +25,7 @@ const WorksComponent = () => {
       const titleElement = titleRef.current;
       gsap.fromTo(
         titleElement,
-        { opacity: 0, y: 45 },
+        { opacity: 0, y: 65 },
         { opacity: 1, duration: 0.8, y: 0, ease: "power2.out", delay: 1.2 }
       );
     }
