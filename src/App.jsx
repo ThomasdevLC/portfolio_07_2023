8532845e415ca20fdbd202659dfabcdbb941dc0c
ThomasdevLC <thomas.lecam@gmail.com>
2023-07-21 15:50:25 +0200

    clean code app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Routes, Route, useLocation } from "react-router-dom";
+import { motion } from "framer-motion";
 import { AnimatePresence } from "framer-motion";
 import LangContext from "./context/LangContext";
 import Home from "./pages/Home";
@@ -14,33 +15,12 @@ import Footer from "./components/Home/Footer";
 import "./App.scss";
 
 const App = () => {
-  const [isDisplayed, setIsDisplayed] = useState(false);
   const [switchLang, setSwitchLang] = useState("fr");
   const location = useLocation();
-
-  console.log(switchLang, "app");
-
-  useEffect(() => {
-    const delay = 300;
-
-    // Utiliser setTimeout pour définir le délai avant d'ajouter la classe "displayed"
-    const timeoutId = setTimeout(() => {
-      setIsDisplayed(true);
-    }, delay);
-
-    // Nettoyage : annuler le timeout lorsque le composant est démonté ou lorsque isDisplayed est true
-    return () => {
-      clearTimeout(timeoutId);
-    };
-  }, []); // Le tableau vide en tant que deuxième argument assure que l'effet n'est exécuté qu'une seule fois lors du montage initial du composant
 
   return (
     <LangContext.Provider value={{ switchLang, setSwitchLang }}>
       <div>
-        <div className={`over ${isDisplayed ? "over-displayed" : ""}`}></div>
-        <div
-          className={`over-top ${isDisplayed ? "over-displayed" : ""}`}
-        ></div>
         <div className="main">
           <AnimatePresence mode="wait">
             <Routes location={location} key={location.pathname}>
