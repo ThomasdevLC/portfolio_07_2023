d0bcb4f53f5b26393efa13d88feda7d12fb08b24
ThomasdevLC <thomas.lecam@gmail.com>
2023-08-23 16:11:51 +0200

    clean code

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import Portfolio from "./pages/works/Portfolio";
 import HeaderNav from "./components/Home/HeaderNav";
 import Footer from "./components/Home/Footer";
 import PreventContext from "./context/PreventContext";
-import transition from "./utils/transitions";
+// import transition from "./utils/transitions";
 
 import "./App.scss";
 
@@ -74,4 +74,4 @@ const App = () => {
   );
 };
 
-export default transition(App);
+export default App;
