93f005f9dec7d9547aa254f0ab2042db63e011f0
ThomasdevLC <thomas.lecam@gmail.com>
2023-08-25 15:28:42 +0200

    footer images

--- src/components/Home/Footer.js
+++ src/components/Home/Footer.js
@@ -1,5 +1,7 @@
 import React from "react";
 import background from "../../assets/images/header_background.jpg";
+import chart from "../../assets/images/chart.png";
+import mark from "../../assets/images/mark.png";
 
 const Footer = () => {
   return (
@@ -10,6 +12,14 @@ const Footer = () => {
       }}
     >
       <div className="footer__border"></div>
+      <div className="footer__box">
+        <div className="footer__box__mark">
+          <img src={mark} alt="mark" />
+        </div>
+        <div className="footer__box__chart">
+          <img src={chart} alt="chart" />
+        </div>
+      </div>
     </div>
   );
 };
