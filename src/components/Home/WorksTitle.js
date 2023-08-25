3cb959a675e057777f60636bd7711687269b3147
ThomasdevLC <thomas.lecam@gmail.com>
2023-08-25 10:51:20 +0200

    new anim title from top

--- src/components/Home/WorksTitle.js
+++ src/components/Home/WorksTitle.js
@@ -4,44 +4,22 @@ import PreventContext from "../../context/PreventContext";
 import homeFr from "../../data/homeText/homeFr";
 import homeEn from "../../data/homeText/homeEn";
 import LangContext from "../../context/LangContext";
-import SvgModule from "../../svg/SvgModule.js"; // Chemin vers le module SVG
-import "../../svg/SvgModule.css"; // Importez le fichier de style
+import SvgModule from "../../svg/SvgModule.js";
+import "../../svg/SvgModule.css";
 
 const WorksTitle = () => {
   const { switchLang } = useContext(LangContext);
   const { preventAnim } = useContext(PreventContext);
-  const arrowRef = useRef(null);
+  const titleRef = useRef(null);
 
   useEffect(() => {
     if (!preventAnim) {
-      // const arrowElement = arrowRef.current;
-
-      // gsap.fromTo(
-      //   arrowElement,
-      //   {
-      //     x: -20,
-      //     opacity: 0,
-      //   },
-      //   {
-      //     x: 0,
-      //     opacity: 1,
-      //     duration: 1,
-      //     ease: ["elastic"],
-      //     delay: 1.6,
-      //   }
-      // );
-
-      const title = SplitType.create(".workstitle__container__text");
-      const splitTitle = title.chars;
-      gsap.from(splitTitle, {
-        y: 25,
-        opacity: 0,
-        duration: 0.8,
-        stagger: 0.02,
-        ease: "back.out",
-        duration: 1,
-        delay: 2.7,
-      });
+      const titleElement = titleRef.current;
+      gsap.fromTo(
+        titleElement,
+        { y: -180 },
+        { duration: 1, y: 0, ease: "power2.out", delay: 2.6 }
+      );
     }
   }, []);
 
@@ -51,22 +29,11 @@ const WorksTitle = () => {
         <SvgModule />
       </div>
       <div className="workstitle">
-        <div className="workstitle__container__text"> PROJETS 2022-2023</div>
-        {/* <div className="workstitle__ellipse">
-          <span
-            class="workstitle__ellipse__arrow material-symbols-outlined"
-            ref={arrowRef}
-          >
-            arrow_right_alt
-          </span>
-        </div>
         <div className="workstitle__container">
-          {switchLang === "fr" ? (
-            <h2 className="workstitle__container__text">{homeFr.title}</h2>
-          ) : (
-            <h2 className="workstitle__container__text">{homeEn.title}</h2>
-          )}
-        </div> */}
+          <p className="workstitle__container__text" ref={titleRef}>
+            PROJETS 2022-2023
+          </p>
+        </div>
       </div>
     </div>
   );
