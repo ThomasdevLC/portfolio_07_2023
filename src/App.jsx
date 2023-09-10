cc8ed1ff8f60f06f8d22aeb1b094fb83acce66c5
ThomasdevLC <thomas.lecam@gmail.com>
2023-09-10 15:39:57 +0200

    clean delete provider

--- src/App.jsx
+++ src/App.jsx
@@ -8,59 +8,51 @@ import Kasa from "./pages/works/Kasa";
 import Pwa from "./pages/works/Pwa";
 import FermeRougeraie from "./pages/works/FermeRougeraie";
 import Portfolio from "./pages/works/Portfolio";
-import ClickedLangContext from "./context/ClickedLangContext";
 import { LangProvider } from "./context/LangContext";
 import { PreventProvider } from "./context/PreventContext";
 import "./App.scss";
 
 const App = () => {
-  const [clickedLang, setClickedLang] = useState(null);
-
   return (
     <LangProvider>
-      <ClickedLangContext.Provider value={{ clickedLang, setClickedLang }}>
-        <PreventProvider>
-          <motion.div
-            className="slide-out"
-            initial={{ scaleY: 1 }}
-            animate={{ scaleY: 0 }}
-            exit={{ scaleY: 0 }}
-            transition={{
-              duration: 1.2,
-              ease: [0.22, 1, 0.36, 1],
-              delay: 1.2,
-            }}
-          ></motion.div>
+      <PreventProvider>
+        <motion.div
+          className="slide-out"
+          initial={{ scaleY: 1 }}
+          animate={{ scaleY: 0 }}
+          exit={{ scaleY: 0 }}
+          transition={{
+            duration: 1.2,
+            ease: [0.22, 1, 0.36, 1],
+            delay: 1.2,
+          }}
+        ></motion.div>
 
-          <div className="main">
-            <AnimatePresence mode="wait">
-              <Routes location={location} key={location.pathname}>
-                <Route
-                  path="/"
-                  element={
-                    <>
-                      <Home />
-                    </>
-                  }
-                ></Route>
-                <Route
-                  path="/ferme-rougeraie/:id"
-                  element={<FermeRougeraie />}
-                ></Route>
-                <Route
-                  path="/groupomania/:id"
-                  element={<Groupomania />}
-                ></Route>
-                <Route path="/kasa/:id" element={<Kasa />}></Route>
-                <Route path="/pwa/:id" element={<Pwa />}></Route>
-                <Route path="/Portfolio/:id" element={<Portfolio />}></Route>
+        <div className="main">
+          <AnimatePresence mode="wait">
+            <Routes location={location} key={location.pathname}>
+              <Route
+                path="/"
+                element={
+                  <>
+                    <Home />
+                  </>
+                }
+              ></Route>
+              <Route
+                path="/ferme-rougeraie/:id"
+                element={<FermeRougeraie />}
+              ></Route>
+              <Route path="/groupomania/:id" element={<Groupomania />}></Route>
+              <Route path="/kasa/:id" element={<Kasa />}></Route>
+              <Route path="/pwa/:id" element={<Pwa />}></Route>
+              <Route path="/Portfolio/:id" element={<Portfolio />}></Route>
 
-                <Route path="*" element={<Home />}></Route>
-              </Routes>
-            </AnimatePresence>
-          </div>
-        </PreventProvider>
-      </ClickedLangContext.Provider>
+              <Route path="*" element={<Home />}></Route>
+            </Routes>
+          </AnimatePresence>
+        </div>
+      </PreventProvider>
     </LangProvider>
   );
 };
--- src/context/ClickedLangContext.js
+++
@@ -1,5 +0,0 @@
-import { createContext } from "react";
-
-const ClickedLangContext = createContext();
-
-export default ClickedLangContext;
