55cf06b34b00d6e2bb44babacf8faa4f3599d4ab
ThomasdevLC <thomas.lecam@gmail.com>
2023-09-11 09:04:31 +0200

    app uselocation router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import { motion } from "framer-motion";
 import { AnimatePresence } from "framer-motion";
 import Home from "./pages/Home";
@@ -13,6 +13,8 @@ import { PreventProvider } from "./context/PreventContext";
 import "./App.scss";
 
 const App = () => {
+  const location = useLocation();
+
   return (
     <LangProvider>
       <PreventProvider>
