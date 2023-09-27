6d34d6d9141f9ab77cfcd4f0e59ad42950d99178
ThomasdevLC <thomas.lecam@gmail.com>
2023-09-27 11:55:28 +0200

    bike shop + farm  images import

--- src/pages/works/BikeShop.js
+++ src/pages/works/BikeShop.js
@@ -1,6 +1,9 @@
 import React, { useContext } from "react";
 import WorksComponent from "../../components/WorksComponent";
 import BackBtn from "../../components/works/BackBtn";
+import bike_anim from "../../assets/images/bike_anim.png";
+import bike_final from "../../assets/images/bike_final.png";
+import bike_shop from "../../assets/images/bike_shop.png";
 import transition from "../../utils/transitions";
 import { PreventContext } from "../../context/PreventContext";
 
@@ -9,9 +12,15 @@ const Portfolio = () => {
   setPreventAnim(true);
 
   return (
-    <div className="portfolio__container">
+    <div className="bikeshop__container">
       <BackBtn />
       <WorksComponent />
+
+      <div className="bikeshop__description">
+        <img src={bike_anim} alt="animation projet bike shop" />
+        <img src={bike_final} alt="page lien boutique projet bike shop" />
+        <img src={bike_shop} alt="page boutique projet bike shop" />
+      </div>
     </div>
   );
 };
